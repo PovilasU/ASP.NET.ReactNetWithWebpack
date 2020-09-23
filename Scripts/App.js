76c5f2936c7c2a613575d3b2c269c9ed20882607
povilasurbonas <povilas.urbonas85@gmail.com>
2020-09-23 08:48:16 +0200

    added and configured webpack

---
+++ Scripts/App.js
@@ -0,0 +1,9 @@
+﻿import { ServerStyleSheet } from 'styled-components';
+global.Styled = { ServerStyleSheet };
+
+
+class Hello extends React.Component {
+    render() {
+        return (<h1> Hello React App</h1>);
+    }
+}
